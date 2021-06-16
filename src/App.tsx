9bb79c126885c07ca9c0b22ac11469d1b134a649
unknown <aquasam93@gmail.com>
2021-06-15 23:33:38 -0400

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import QuestionCard from './components/QuestionCard';
+import {fetchQuizQuestions} from './API'
+import {Difficulty} from './API'
 
-function App() {
+const TOTAL = 10;
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions,setQuestions] = useState([]);
+  const [number, setNumber] = useState(1);
+  const [answer, setAnswer] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log('data',fetchQuizQuestions(TOTAL,Difficulty.EASY));
+
+
+  const startQuiz = async() => {
+    
+  }
+
+  const checkAns = (e: React.MouseEvent<HTMLButtonElement>) =>{
+
+  }
+
+  const nextQuestion = () =>{
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startQuiz}>START</button>
+      <p className="score">Score: </p>
+      <p>Loading Question ......</p>
+      {/* <QuestionCard 
+      questionNr = {number}
+      totalQuestions={TOTAL}
+      question = {questions[number].question}
+      answer = {questions[number].answers}
+      userAnswer = {answer? answer[number] : undefined} 
+      callback = {checkAns}/> */}
+      
+
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
 
 export default App;
+function async() {
+  throw new Error('Function not implemented.');
+}
+
